faf69b3b63e36ac8b49180444e11ddac9e476642
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-21 12:44:42 -0300

    JS Comments concept.

---
+++ js-tutorial/comments/script.js
@@ -0,0 +1,29 @@
+// JavaScript Comments
+
+/*
+Single line comments start with //.
+
+Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
+This example uses a single-line comment before each code line: */
+
+// Change heading:
+document.getElementById("myH").innerHTML = "My First Page";
+
+// Change paragraph:
+document.getElementById("myP").innerHTML = "My first paragraph.";
+console.log("This example uses a single line comment at the end of each line to explain the code:");
+
+let x = 5;      // Declare x, give it th  e value of 5
+let y = x + 2;  // Declare y, give it the value of x + 2
+
+document.getElementById("result").innerHTML = y;
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+*/
+document.getElementById("myH").innerHTML = "My First Page";
+document.getElementById("myP").innerHTML = "My first paragraph.";
