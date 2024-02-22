64cc5c23164e46f1c79aebc7ea535f53cf6201c2
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 10:12:02 -0300

    JS Variables Concept: Understanding case sensitive.

---
+++ js-tutorial/variables/declare-case-sensitive.js
@@ -0,0 +1,32 @@
+// JavaScript Identifiers
+
+// All JavaScript variables must be identified with unique names.
+
+// These unique names are called identifiers.
+
+// Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+// The general rules for constructing names for variables (unique identifiers) are:
+
+// Names can contain letters, digits, underscores, and dollar signs.
+let name = "Otavio";
+let complete_name = "Otavio Campagnoli";
+let $name = "Otavio";
+let n4me = "n4me";
+// Names must begin with a letter.
+// Names can also begin with $ and _ .
+let _test = "test";
+let $test = "another test";
+// Names are case sensitive (x and X are different variables).
+let x = "x";
+let X = "X";
+// Reserved words (like JavaScript keywords) cannot be used as names.
+
+console.log(name);
+console.log(complete_name);
+console.log($name);
+console.log(n4me);
+console.log(_test);
+console.log($test);
+console.log(x);
+console.log(X);
