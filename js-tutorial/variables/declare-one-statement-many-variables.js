2943c4810ed38cf6373c512514949ced1aa386c4
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 12:02:00 -0300

    JS Variables Concept: Declare One Statement, many Variables.

---
+++ js-tutorial/variables/declare-one-statement-many-variables.js
@@ -0,0 +1,14 @@
+/*One Statement, Many Variables */
+
+// You can declare many variables in one statement.
+
+// Start the statement with let and separate the variables by comma:
+
+let person = "Otavio Henrique", carName = "Fusca", price = 2000;
+
+// A declaration can span multiple lines:
+//Example:
+let guess = "Henrique Otavio",
+car = "Uno",
+model = "Fiat",
+value = 500;
