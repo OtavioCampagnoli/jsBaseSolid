7625ce0fa4f9b4c10e2f48b7d83904b56c40ab2a
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 20:30:41 -0300

    JS Let Concept: Understading browsers are not supported with let and const keywords.

---
+++ js-tutorial/let/browser-support.js
@@ -0,0 +1,8 @@
+// Browser Support
+
+// The let and const keywords are not supported in Internet Explorer 11 or earlier.
+
+// The following table defines the first browser versions with full support:
+				
+// Chrome 49 	Edge 12 	Firefox 36 	Safari 11 	Opera 36
+// Mar, 2016 	Jul, 2015 	Jan, 2015 	Sep, 2017 	Mar, 2016
