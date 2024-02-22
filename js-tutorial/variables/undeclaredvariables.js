e66d6f539d4609c245c5724b955426c1076f9ac5
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-21 21:06:03 -0300

    JS Variables Concept: Undeclared variables.

---
+++ js-tutorial/variables/undeclaredvariables.js
@@ -0,0 +1,20 @@
+/*Variables are Containers for Storing Data
+
+JavaScript Variables can be declared in 4 ways:
+
+- Automatically;
+- Using var
+- Using let
+- Using const
+
+In thi first example, x, y and z are undeclared variables.
+
+They area automatically declared when first used:
+*/
+
+x = 5;
+y = 6;
+z = x + y;
+console.log(z);
+
+// Note: It is considered good programming practice to always declare variables before use.
