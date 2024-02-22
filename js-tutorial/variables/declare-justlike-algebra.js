22e8db26711afd57ca17fc35b593f44da27877c1
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 10:10:30 -0300

    JS Variables Concept: Declare using expressions, just like algebra.

---
+++ js-tutorial/variables/declare-justlike-algebra.js
@@ -0,0 +1,14 @@
+let numberOne = 2;
+let numberTwo = 3;
+let result = numberOne + numberTwo;
+console.log(result);
+
+// Just like Algebra
+
+/*
+    variables are used in expressions:
+
+    from the example above, you can guess that the total is calculated to be 5;
+
+    Note: Variables are containers for storing values.
+*/
