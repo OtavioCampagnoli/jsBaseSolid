df2a97fdf2537705855949f122c227447d2c0438
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 13:14:05 -0300

    Concept of JS Variables: Working with joining different data types.

---
+++ js-tutorial/variables/arithmetic.js
@@ -0,0 +1,31 @@
+/*
+JavaScript Arithmetic
+
+As with algebra, you can do arithmetic with JavaScript variables, using operators like = and +: 
+
+*/
+
+let x = 5 + 3 + 2;
+console.log(x);
+console.log(typeof x);
+// You can also add strings, but strings will be concatenated:
+
+let y = "Otavio" + " " + "Henrique";
+console.log(y);
+console.log(typeof y);
+
+// Also try this:
+
+let d = "5" + 3 + 2;
+console.log(d);
+console.log(typeof d);
+
+/*
+Note
+
+If you put a number in quotes, the rest of the numbers will be treated as strings, and concatenated.
+ */
+
+let e = 3 + 2 + "5";
+console.log(e);
+console.log(typeof e);
