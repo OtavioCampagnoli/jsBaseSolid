b85d55c59000439717b7fdc955795e5e0b94dd3c
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 12:53:44 -0300

    JS Variables Concept: Re-Declaring Variables.

---
+++ js-tutorial/variables/re-declaring-variables.js
@@ -0,0 +1,15 @@
+/*
+    Re-Declaring JavaScript Variables
+
+If you re-declare a JavaScript variable declared with var, it will not lose its value.
+
+The variable carName will still have the value "Volvo" after the execution of these statements:
+/*/
+var carName = "Volvo";
+var carName;
+
+console.log(carName);
+
+var name = "name";
+
+console.log(name);
