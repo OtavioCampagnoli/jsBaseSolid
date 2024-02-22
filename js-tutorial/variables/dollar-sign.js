41cbac76f9ca20ffdd136f3133be255456f18cd8
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 13:25:14 -0300

    Concept of JS Variables: Working with dollar sign in JS.

---
+++ js-tutorial/variables/dollar-sign.js
@@ -0,0 +1,19 @@
+/*
+JavaScript Dollar Sign $
+
+Since JavaScript treats a dollar sign as a letter, identifiers containing $ are valid variable names:
+*/
+
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
+
+console.log($);
+console.log($$$);
+console.log($myMoney);
+
+/*
+
+Using the dollar sign is not very common in JavaScript, but professional programmers often use it as an alias for the main function in a JavaScript library.
+
+In the JavaScript library jQuery, for instance, the main function $ is used to select HTML elements. In jQuery $("p"); means "select all p elements".*/
