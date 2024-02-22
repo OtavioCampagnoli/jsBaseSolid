96630dbfcfe762cd15739fdbacb60bbbbedb8e66
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 11:02:49 -0300

    JS Variables Concept: Declare variables and show Data types.

---
+++ js-tutorial/variables/declare-datatypes.js
@@ -0,0 +1,21 @@
+// JavaScript Data Types
+
+// JavaScript variables can hold numbers like 100 and text values like "John Doe".
+
+// In programming, text values are called text strings.
+
+// JavaScript can handle many types of data, but for now, just think of numbers and strings.
+
+// Strings are written inside double or single quotes. Numbers are written without quotes.
+
+// If you put a number in quotes, it will be treated as a text string.
+// Example
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes I am!';
+
+console.log(typeof(pi));
+console.log(typeof(person));
+console.log(typeof(answer));
+
+// typeof() is a function to show what is the datatype, what you want to know.
