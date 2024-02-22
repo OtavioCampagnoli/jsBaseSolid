716b20bf9fcc690f1c1ebafcf91eb312c5c92533
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-21 21:32:07 -0300

    JS Variables Concept: Declare mixed.

---
+++ js-tutorial/variables/declare-mixed.js
@@ -0,0 +1,29 @@
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
+
+/*
+The two variables price1 and price2 are declared with the const keyword.
+
+These are constant values and cannot be changed.
+
+The variable total is declared with the let keyword.
+
+The value total can be changed. */
+
+
+/*
+When to Use var, let, or const?
+
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers.
+
+*/
