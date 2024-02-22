70f96c06b4a5c30e823ce4f28a214ad0ffc4f282
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-21 21:07:25 -0300

    JS Variables Concept: Declare with var.

---
+++ js-tutorial/variables/declare-with-var.js
@@ -0,0 +1,19 @@
+/*
+    From the examples you can guess:
+
+    x stores the value 5
+    y stores the value 6
+    z stores the value 11
+ */
+var x = 5;
+var y = 6;
+var z = x + y;
+
+/*
+NOTE:
+The var keyword was used in all JavaScript code from 1995 to 2015.
+
+The let and const keywordavaScript in 2015.s were added to J
+
+The var keyword should only be used in code written for older browsers.
+ */
