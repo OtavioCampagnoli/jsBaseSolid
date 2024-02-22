353ae289251aeaf04ef7825cc4f45e1ce428972c
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 13:54:45 -0300

    JS Variables Concept: Exercices to apply my knowledge.

---
+++ js-tutorial/variables/exercise2.js
@@ -0,0 +1,4 @@
+// Create a variable called x, assign the value 50 to it.
+
+let x = 50;
+console.log(x);
---
+++ js-tutorial/variables/exercise5.js
@@ -0,0 +1,9 @@
+// On one single line, declare three variables with the following names and values:
+
+// firstName = "John"
+// lastName = "Doe"
+// age = 35 
+
+let firstName = "John", lastName = "Doe", age = 35;
+console.log(firstName + " " + lastName);
+console.log(age);
