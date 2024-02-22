2f95ba94d16e04be72d6440847451cc55f3d3694
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-22 13:44:19 -0300

    Concept of JS Variables: Declare Variables with underscore (_).

---
+++ js-tutorial/variables/underscore.js
@@ -0,0 +1,13 @@
+// JavaScript Underscore (_)
+
+// Since JavaScript treats underscore as a letter, identifiers containing _ are valid variable names:
+
+let _lastName = "Campagnoli";
+let _x = 2;
+let _100 = 5;
+
+console.log(_lastName);
+console.log(_x);
+console.log(_100);
+
+// Using the underscore is not very common in JavaScript, but a convention among professional programmers is to use it as an alias for "private (hidden)" variables.
