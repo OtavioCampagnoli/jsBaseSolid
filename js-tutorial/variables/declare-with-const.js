dc237f71dcc0ea039c0e795d61a5f9817e90c2e9
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-21 21:31:50 -0300

    JS Variables Concept: Declare with const.

---
+++ js-tutorial/variables/declare-with-const.js
@@ -0,0 +1,8 @@
+const x = 5;
+const y = 6;
+const z = x + y;
+console.log(z);
+
+// Always use const if the type should not be changed (Arrays and Objects);
+
+// Always use const if the value should not be changed;
