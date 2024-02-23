1cd270592fcd73b1a8cb2714915d3e61fa913e03
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-23 10:22:24 -0300

    JS Const Concept: Must be assigned and when to use const keyword.

---
+++ js-tutorial/const/must-be-assigned.js
@@ -0,0 +1,25 @@
+/*
+    Must be Assigned
+
+    JavaScript const variables must be assigned a value when they are declared:0
+*/
+
+const pi = 3.14159265359; // corret way
+
+// const interestRate;
+// interestRate = 1.133; // incorret way
+
+/*
+
+When to use JavaScript const?
+
+Always declare a variable with const when you know that the value should not be changed.
+
+Use const when you declare:
+
+    A new Array
+    A new Object
+    A new Function
+    A new RegExp
+
+*/
