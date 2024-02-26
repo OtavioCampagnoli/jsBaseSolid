36bf8998cd61f2ed2494754ff74c9ca0103c5656
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-26 14:59:37 -0300

    JS Operators Concept: Using assignment operators.

---
+++ js-tutorial/operators/assignment-operators.js
@@ -0,0 +1,24 @@
+// JavaScript Assignment Operators
+
+// Assignment operators assign values to JavaScript variables.
+
+// The Addition Assignment Operator (+=) adds a value to a variable.
+
+// Operator 	Example 	Same As
+// = 	        x = y 	    x = y
+// += 	        x += y 	    x = x + y
+// -= 	        x -= y 	    x = x - y
+// *= 	        x *= y 	    x = x * y
+// /= 	        x /= y 	    x = x / y
+// %= 	        x %= y 	    x = x % y
+// **= 	        x **= y     x = x ** y
+
+let x = 10;
+console.log(x += 5); // 15
+console.log(x -= 5); // 10
+console.log(x *= 5); // 50
+console.log(x /= 5); // 2
+console.log(x %= 5); // 0
+console.log(x **= 2); // 0
+x = 10;
+console.log(x **= 2); // 100
