ab67775a289efbefa5d2a5aa9e28e39171a52df6
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-26 15:50:09 -0300

    JS Operators Concept: Adding Strings and Numbers.

---
+++ js-tutorial/operators/adding-strings-and-numbers.js
@@ -0,0 +1,17 @@
+// Adding Strings and Numbers
+
+// Adding two numbers, will return the sum, but adding a number and a string will return a string:
+
+let x = 5 + 5;
+let y = "5" + 100;
+let z = "Hello" + 5;
+
+console.log(x); // 10
+console.log(y); // 5100
+console.log(z); // Hello5
+
+// Note
+
+// If you add a number and a string, the result will be a string!
+
+console.log(typeof z); // for example in this case... the type of z will be return string
