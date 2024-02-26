ec98d5d5dc722b7bed32f7731b91b214b891a9a8
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-26 13:40:51 -0300

    JS Operators Concept: List of operators and types of operators.

---
+++ js-tutorial/operators/operators-and-types-of-operators.js
@@ -0,0 +1,19 @@
+// JavaScript Operators
+
+// The Addition Operator + adds numbers:
+
+// The Assignment Operator = assigns a value to a variable.
+
+
+// Types of JavaScript Operators
+
+// There are different types of JavaScript operators:
+
+//     Arithmetic Operators
+//     Assignment Operators
+//     Comparison Operators
+//     String Operators
+//     Logical Operators
+//     Bitwise Operators
+//     Ternary Operators
+//     Type Operators
