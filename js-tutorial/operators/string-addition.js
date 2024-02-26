cb858e007550c3736bd60765d8aa776a4e2c5187
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-26 15:30:32 -0300

    JS Operators Concept: String addition.

---
+++ js-tutorial/operators/string-addition.js
@@ -0,0 +1,19 @@
+// JavaScript String Addition
+
+// The + can also be used to add (concatenate) strings:
+
+let firstName = "Otávio";
+let middleName = "Henrique Moreira";
+let lastName = "Campagnoli";
+const fullName = firstName + " " + middleName + " " + lastName;
+console.log(fullName); // Otávio Henrique Moreira Campagnoli
+
+// The += assignment operator can also be used to add (concatenate) strings:
+
+niceDay = "Have a nice day! ";
+niceDay += fullName;
+console.log(niceDay);
+
+// Note:
+
+// When used on strings, the + operator is called the concatenation operator.
