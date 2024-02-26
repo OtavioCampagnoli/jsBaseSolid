51174258626801434096cacd80ace4df78c5fd90
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-26 14:28:38 -0300

    JS Operators Concept: Arithmetic operators.

---
+++ js-tutorial/operators/arithmetic.js
@@ -0,0 +1,19 @@
+// JavaScript Arithmetic Operators
+
+// Arithmetic Operators are used to perform arithmetic on numbers:
+
+let a = 3;
+
+let x = (100 + 50) * a;
+
+console.log(x);
+
+// Operator 	Description
+// + 	        Addition
+// - 	        Subtraction
+// * 	        Multiplication
+// ** 	        Exponentiation (ES2016)
+// / 	        Division
+// % 	        Modulus (Division Remainder)
+// ++ 	        Increment
+// -- 	        Decrement
