ca93183a9d4925b4f9bd63ade16578d06739d23b
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2024-02-26 16:17:21 -0300

    JS Operators Concept:  Using Logical operators.

---
+++ js-tutorial/operators/logical-operators.js
@@ -0,0 +1,13 @@
+// JavaScript Logical Operators
+// Operator 	Description
+// && 	        logical and
+// || 	        logical or
+// ! 	        logical not
+
+let x = 10;
+let y = 20;
+let z = 30;
+console.log(x == y && z >= 10); // false
+console.log(y >= x && z != x); // true
+console.log(x == y || z >= 10); // true
+console.log(!(x == 10)); //false
