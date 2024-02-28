220c285463bec8cb6cb6b7f20eafbf01e3e6d519
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-28 12:26:56 -0300

    JS Data Types Concept: Working with some Datatype and Object Datatype.

---
+++ js-tutorial/data-types/data-types.js
@@ -0,0 +1,61 @@
+// JavaScript Data Types
+
+/* JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+
+The Object Datatype
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date */
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log(length);
+console.log(weight);
+// Strings:
+
+let color = "Red";
+let firstName = "Otavio";
+console.log(color);
+console.log(firstName);
+
+// Booleans
+
+let x = true;
+let y = false;
+console.log(x);
+console.log(y);
+
+// Object
+
+const person = {
+  firstName: "Otávio",
+  lastName: "Campagnoli",
+};
+
+console.log(person);
+
+// Array object:
+
+const car = ["Gol", "Fusca", "Corsa"];
+
+console.log(car);
+
+// Date object:
+
+const date = new Date("2024-02-28");
+console.log(date);
+
+/*  Note: 
+    A JavaScript variable can hold any type of data.
+ */
