ba97c5e567bbddae1ec31ff136ff933b2c467123
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-02-28 13:03:04 -0300

    JS Data Types Concept: Types Dynamic.

---
+++ js-tutorial/data-types/types-dynamic.js
@@ -0,0 +1,9 @@
+/* JavaScript Types are Dynamic
+JavaScript has dynamic types. This means that the same variable can be used to hold different data types: */
+
+let x;
+console.log(typeof x);
+x = 500;
+console.log(typeof x);
+x = "Otavio";
+console.log(typeof x);
