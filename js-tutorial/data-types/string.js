68636fbe155715aa8c414b20d41b1d12ff2214e8
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 12:23:52 -0300

    JS Data Types Concept: Strings

---
+++ js-tutorial/data-types/string.js
@@ -0,0 +1,16 @@
+// A string (or a text string) is a series of characters like "John Doe".
+
+// Strings are written with quotes. You can use single or double quotes:
+
+let carName1 = "Fusca";
+let carName2 = "Honda Fit";
+let carName3 = "Ferrari";
+
+// you can use quotes inside a string, as long as they dont match the quotes surrounding the string:
+
+let answer1 = "It's great";
+let answer2 = "He is called 'Otavio'";
+let answer3 = 'He is called "Otavio"';
+
+console.log(carName1 + ", " + carName2 + ", " + carName3);
+console.log(answer1 + ", " + answer2 + ", " + answer3);
