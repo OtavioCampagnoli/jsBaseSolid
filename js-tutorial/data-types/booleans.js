c783cb164337b5824eb5e7d8f6777f7534371c97
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 12:45:11 -0300

    JS Data Types Concept: Booleans.

---
+++ js-tutorial/data-types/booleans.js
@@ -0,0 +1,12 @@
+// JavaScript Booleans
+
+// Booleans can only have two values: true or false.
+
+let x = 10;
+let y = 10;
+let z = 30;
+
+console.log((x == y)); // true
+console.log((x == z)); // false
+
+// Booleans are often used in conditional testing.
