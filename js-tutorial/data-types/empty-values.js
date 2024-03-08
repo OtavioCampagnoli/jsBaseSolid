d40d1bbe75ec5d15885b249af7b2c74571315b45
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 14:58:51 -0300

    JS Data Types Concept: Empty values.

---
+++ js-tutorial/data-types/empty-values.js
@@ -0,0 +1,10 @@
+// Empty Values
+
+// An empty value has nothing to do with undefined.
+
+// An empty string has both a legal value and a type.
+
+let car = "";
+
+console.log(typeof car === "string");
+console.log(car);
