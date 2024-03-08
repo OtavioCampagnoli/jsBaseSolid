f2a3bb1ff4eebd606e97c8fd0fc7eaa65574562f
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 14:54:28 -0300

    JS Data Types Concept: typeof.

---
+++ js-tutorial/data-types/typeof.js
@@ -0,0 +1,14 @@
+// The typeof Operator
+
+// You can use the JavaScript typeof operator to find the type of a JavaScript variable.
+
+// The typeof operator returns the type of a variable or an expression:
+console.log(typeof ""); // Resturns "string"
+console.log(typeof "Otavio"); // Returns "string"
+console.log(typeof "Otavio Campagnoli"); // Returns "string"
+
+console.log(typeof 0); // Returns "number"
+console.log(typeof 314); // Returns "number"
+console.log(typeof 3.14); // Returns "number"
+console.log(typeof 3); // Returns "number"
+console.log(typeof (3 + 4)); // Returns "number"
