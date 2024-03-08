42b0ae5b9ac2fb4e99741967b9402ca697102f0e
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 14:51:11 -0300

    JS Data Types Concept: Objects.

---
+++ js-tutorial/data-types/objects.js
@@ -0,0 +1,22 @@
+// JavaScript Objects
+
+// JavaScript objects are written with curly braces {}.
+
+// Object properties are written as name:value pairs, separated by commas.
+
+const person = {
+  firstName: "Otavio",
+  lastName: "Campagnoli",
+  age: 20,
+  eyeColor: "brown",
+};
+
+console.log(
+  person.firstName +
+    " " +
+    person.lastName +
+    " " +
+    person.age +
+    " " +
+    person.eyeColor
+); //Otavio Campagnoli 20 brown
