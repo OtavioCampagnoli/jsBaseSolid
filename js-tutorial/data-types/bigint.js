e6182e90ca1c124106c1587f28af563c62a07ab5
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 12:35:20 -0300

    JS Data Types Concept: BigInt ES2020

---
+++ js-tutorial/data-types/bigint.js
@@ -0,0 +1,8 @@
+// JavaScript BigInt
+
+// All JavaScript numbers are stored in a a 64-bit floating-point format.
+
+// JavaScript BigInt is a new datatype (ES2020) that can be used to store integer values that are too big to be represented by a normal JavaScript Number.
+
+let x = BigInt("123456789012345678901234567890");
+console.log(x); // 123456789012345678901234567890n
