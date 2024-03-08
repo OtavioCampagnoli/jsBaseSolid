884ff5c018278276a6b733c00f9bfdbab59b0306
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 14:47:10 -0300

    JS Data Types Concept: Arrays.

---
+++ js-tutorial/data-types/arrays.js
@@ -0,0 +1,16 @@
+// JavaScript Arrays
+
+// JavaScript arrays are written with square brackets [].
+
+// Array items are separated by commas (,).
+
+// The following code declares (creates) an array called cars, containing three items (car names):
+
+const car = ["Fusca", "Honda Fit", "Ferrari"];
+console.log(car);
+
+// Array indexes are zero-based, which means the first item is [0], second is [1], and so on.
+
+console.log(car[0]);
+console.log(car[1]);
+console.log(car[2]);
