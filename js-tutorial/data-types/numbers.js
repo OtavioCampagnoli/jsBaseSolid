af247bbe86ed4c260c60dfc6caf20f511f23e7a3
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 12:30:42 -0300

    JS Data Types Concept: Numbers

---
+++ js-tutorial/data-types/numbers.js
@@ -0,0 +1,12 @@
+// JavaScript Numbers
+
+// All JavaScript numbers are stored as decimal numbers (floating point).
+
+// Numbers can be written with, or without decimals:
+
+let x1 = 34.01;
+
+let x2 = 34;
+
+console.log(x1); // 34.01
+console.log(x2); // 34
