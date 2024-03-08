dd64458c93e8c592eefaed55a96f1c3fa4b843ab
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 12:32:08 -0300

    JS Data Types Concept: Exponential Notation.

---
+++ js-tutorial/data-types/exponential-notation.js
@@ -0,0 +1,23 @@
+// Exponential Notation
+
+// Extra large or extra small numbers can be written with scientific (exponential) notation:
+
+let y = 123e5;
+
+let z = 123e-5;
+
+console.log(y); // 12300000
+console.log(z); // 0.00123
+
+// Note
+
+// Most programming languages have many number types:
+
+// Whole numbers (integers):
+// byte (8-bit), short (16-bit), int (32-bit), long (64-bit)
+
+// Real numbers (floating-point):
+// float (32-bit), double (64-bit).
+
+// Javascript numbers are always one type:
+// double (64-bit floating point).
