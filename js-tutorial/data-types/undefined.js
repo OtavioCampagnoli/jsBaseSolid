fa9e16a26e181ae5e992d6e35028c46d82c59af8
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-08 14:56:20 -0300

    JS Data Types Concept: undefined.

---
+++ js-tutorial/data-types/undefined.js
@@ -0,0 +1,11 @@
+// Undefined
+
+// In JavaScript, a variable without a value, has the value undefined. The type is also undefined.
+
+let car;
+console.log(car);
+
+// Any variable can be emptied, by setting the value to undefined. The type will also be undefined.
+
+car = undefined;
+console.log(car);
