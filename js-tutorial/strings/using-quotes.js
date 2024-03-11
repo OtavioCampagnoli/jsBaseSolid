80f5fc1e05c9e5ae63b46c3ab56d32198e00087d
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-11 13:29:03 -0300

    JS Strings Concept: Using Quotes.

---
+++ js-tutorial/strings/using-quotes.js
@@ -0,0 +1,20 @@
+// JavaScript Strings
+
+// Strings are for storing text
+
+// Strings are written with quotes
+
+// Using Quotes
+
+// A JavaScript string is zero or more characters written inside quotes.
+
+let text = "Otavio Campagnoli";
+
+let carName1 = 'Fiat Uno';
+let carName2 = "Fiat Uno";
+
+// Note
+
+// Strings created with single or double quotes works the same.
+
+// There is no difference between the two.
