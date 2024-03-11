056721ee539d8efed66a3c95a6fce4b14fcac333
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-11 14:21:21 -0300

    JS Strings Concept: Breaking Long Lines.

---
+++ js-tutorial/strings/breaking.js
@@ -0,0 +1,13 @@
+// Breaking Long Lines
+
+// For readability, programmers often like to avoid long code lines.
+
+// A safe way to break up a statement is after an operator:
+
+document.getElementById("demo").innerHTML = 
+"Hello Otavio!";
+
+// A safe way to break up a string is by using string addition:
+
+document.getElementById("Demo").innerHTML = "Hello " + 
+"Otavio!";
