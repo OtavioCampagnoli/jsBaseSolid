1ea9358a96e91975c0b508194fcf12febd19137c
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-11 10:52:11 -0300

    JS Objects Concept: Introduction of objects concept using car example.

---
+++ js-tutorial/objects/car.js
@@ -0,0 +1,25 @@
+// Real Life Objects, Properties, and Methods
+
+// In real life, a car is an object.
+
+// A car has properties like weight and color, and methods like start and stop:
+
+// All cars have the same properties, but the property values differ from car to car.
+
+// All cars have the same methods, but the methods are performed at different times.
+
+let oneCar = "Fiat";
+
+// Objects are variables too. But objects can contain many values.
+
+// example
+
+const car = { type: "Fiat", model: "Uno", color: "White" };
+
+console.log(car.type); // Fiat
+console.log(car.model); // Uno
+console.log(car.color); // White
+
+// The values are written as name:value pairs (name and value separated by a colon).
+
+// It is a common practice to declare objects with the const keyword.
