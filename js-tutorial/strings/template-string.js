3cf1dee80cd05142db2e32a7a37c41f7abbc6bbf
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-11 13:33:20 -0300

    JS Strings Concept: Using Template Strings.

---
+++ js-tutorial/strings/template-string.js
@@ -0,0 +1,14 @@
+// Template Strings
+
+// Templates were introduced with ES6 (JavaScript 2016).
+
+// Templates are strings enclosed in backticks (`This is a template string`).
+
+// Templates allow single and double quotes inside a string:
+
+let text = `he's often called "Otavius".`;
+console.log(text);
+
+// Note:
+
+// Templates are not supported in Internet Explorer.
