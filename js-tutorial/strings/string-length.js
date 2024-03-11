606281353c9631545fbbce468b1db64866d2a357
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-11 13:36:55 -0300

    JS Strings Concept: String Length prop.

---
+++ js-tutorial/strings/string-length.js
@@ -0,0 +1,8 @@
+// String Length
+
+// To find the length of a string, use the built-in length property:
+
+let text = "ABCDEFGHIJHLMNOPQRSTUVWXYZ";
+
+let length  = text.length;
+console.log(length);
