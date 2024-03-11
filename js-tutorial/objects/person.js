ef02c480267fa11ebfa6a71c6a67e161c9bc3499
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-11 11:03:25 -0300

    JS Objects Concept: Object Definition using person example.

---
+++ js-tutorial/objects/person.js
@@ -0,0 +1,13 @@
+// Object Definition
+
+// You define (and create) a JavaScript object with an object literal:
+
+const person = {firstName: "Otavio", lastName: "Campagnoli", age: 20, eyeColor: "Brown"};
+// accessing Object Properties:
+// Have two ways to access properties of object:
+
+// first way
+console.log(person.firstName);
+console.log(person["firstName"]);
+
+// Spaces and line breaks are not important. An object definition can span multiple lines: 
