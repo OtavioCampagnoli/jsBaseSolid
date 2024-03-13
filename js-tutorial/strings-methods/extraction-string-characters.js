e8fb87b93d82f145bfb33e4195cf71008093c343
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-13 19:11:23 -0300

    JS String Methods Concept: Extraction String Characters

---
+++ js-tutorial/strings-methods/extraction-string-characters.js
@@ -0,0 +1,9 @@
+// Extracting String Characters
+
+// There are 4 methods for extracting string characters:
+
+//     The at(position) Method
+//     The charAt(position) Method
+//     The charCodeAt(position) Method
+//     Using property access [] like in arrays
+
